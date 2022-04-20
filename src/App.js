f8abadf7f5cc9967b9d145eec3b8d2a30be1bf8f
Marek Zdybel <marek.zdybel@sbg.com.pl>
2022-04-20 17:25:09 +0200

    React loop simple example

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+
+
+const warnings = 
+    [
+      {id: 1, type: "Warning 1", date: "2022-04-30"},
+      {id: 2, type: "Warning 2", date: "2022-05-30"},
+      {id: 3, type: "Warning 3", date: "2022-06-30"}
+    ];
+
+const alerts = 
+    [
+      {id: 1, type: "Alerts 1", date: "2022-07-30"},
+      {id: 2, type: "Alerts 2", date: "2022-08-30"},
+      {id: 3, type: "Alerts 3", date: "2022-09-30"}
+    ];
+
+
+
+function App(props) {
+
+  // const warningList = (
+  //   <ul>
+  //     {props.pos}
+  //   </ul>
+  // );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,65 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+const warnings = 
+    [
+      {id: 1, type: "Warning 1", date: "2022-04-30"},
+      {id: 2, type: "Warning 2", date: "2022-05-30"},
+      {id: 3, type: "Warning 3", date: "2022-06-30"}
+    ];
+
+const alerts = 
+    [
+      {id: 1, type: "Alerts 1", date: "2022-07-30"},
+      {id: 2, type: "Alerts 2", date: "2022-08-30"},
+      {id: 3, type: "Alerts 3", date: "2022-09-30"}
+    ];
+
+
+const messages = [
+  { warnings: warnings },
+  { alerts: alerts } 
+];
+
+function App(props) {
+
+  const warnings = props.messages[0].warnings;
+  const alerts = props.messages[1].alerts;
+  
+  const warningList = (
+    <ul>
+      {warnings.map((warning) => 
+      <li key={warning.id}>
+        <p>Date: {warning.date} <span>{warning.type}</span></p>
+      </li>
+      )}
+    </ul>
+  );
+
+  const alertList = (
+    <ul>
+      {alerts.map((alert) => 
+      <li key={alert.id}>
+        <p>Date: {alert.date} <span> {alert.type} </span></p>
+      </li>
+      )}
+    </ul>
+  );
+
+  return (
+    <div>
+      {warningList}
+    <hr />
+      {alertList}
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <App messages={ messages } />
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
